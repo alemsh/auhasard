b2a6f72a1258fb925e79a52d2bb8714819494fee
alemsh <alecmshep@gmail.com>
2025-12-24 12:03:22 -0600

    redo how the trs are found and grouped

--- auhasard.go
+++ auhasard.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
-	//"io"
+	"os"
+	"regexp"
+	"slices"
+	"strings"
+
 	"errors"
 	"log"
 	"net/http"
@@ -12,23 +16,24 @@ import (
 
 // A translation is collection of interpretations
 type translation struct {
-	interpretation map[string]interpretation
+	interpretations []*interpretation
 }
 
 type interpretation struct {
-	from    word
-	to      []word
-	context []context
-	kind    string
+	from     []*word
+	to       []*word
+	examples []*example
+	kind     string
 }
 
 type word struct {
-	name     string
-	language string
-	pos      string
+	name       string
+	language   string
+	pos        string
+	definition string
 }
 
-type context struct {
+type example struct {
 	language string
 	phrase   string
 }
@@ -38,24 +43,94 @@ type WRParser interface {
 	parseWRDTable()
 }
 
-func parseRoot(root *goquery.Selection) *translation {
-	root.Each(func(i int, s *goquery.Selection) {
-		tType, err := selectTranslationType(s, t)
+func newTranslation() *translation {
+	t := translation{
+		interpretations: make([]*interpretation, 0),
+	}
+	return &t
+}
+
+func newInterpretation() *interpretation {
+	i := interpretation{
+		from:     make([]*word, 0),
+		to:       make([]*word, 0),
+		examples: make([]*example, 0),
+	}
+	return &i
+}
+
+func parseTables(tables *goquery.Selection) *translation {
+	t := newTranslation()
+	tables.Each(func(i int, table *goquery.Selection) {
+		kind, err := selectTranslationKind(table)
 		if err != nil {
 			log.Fatal(err)
 		}
-		t.interpretation.parseWRDTable(s)
+		groups := GroupEvenOddRows(table)
+		for _, group := range groups {
+			interp := newInterpretation()
+			interp.kind = kind
+			err := parseTRs(group, interp)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t.interpretations = append(t.interpretations, interp)
+		}
 	})
-
 	return t
 }
 
-func selectTranslationType(s *goquery.Selection, t *translation) (string, error) {
-	tType, exists := s.Find("tr.wrtopsection").Attr("data-ph")
-	if exists != true {
+func GroupEvenOddRows(table *goquery.Selection) []*goquery.Selection {
+	var groups []*goquery.Selection
+	var currentGroup *goquery.Selection
+
+	table.Find("tr").Each(func(i int, tr *goquery.Selection) {
+		class, _ := tr.Attr("class")
+
+		if goquery.NodeName(tr) != "tr" {
+			return
+		}
+
+		if !slices.Contains([]string{"odd", "even"}, class) {
+			return
+		}
+		// Start a new group if none is active
+		if currentGroup.Length() == 0 {
+			currentGroup = &goquery.Selection{}
+			currentGroup = currentGroup.AddSelection(tr)
+		} else {
+			prevClass, _ := currentGroup.Last().Attr("class")
+			if class != prevClass {
+				groups = append(groups, currentGroup)
+				currentGroup = nil
+			} else {
+
+			}
+			currentGroup = currentGroup.AddSelection(tr)
+		}
+
+	})
+	return groups
+}
+
+func containsClass(classAttr, className string) bool {
+	for _, c := range strings.Fields(classAttr) {
+		if c == className {
+			return true
+		}
+	}
+	return false
+}
+
+func selectTranslationKind(s *goquery.Selection) (string, error) {
+	row := s.Find("tr.wrtopsection")
+	fmt.Printf("%v\n\n", row.Text())
+	kind, exists := row.Find("span.ph").Attr("data-ph")
+	row.Remove()
+	if !exists {
 		return "", errors.New("tr element with class=wrtopsection and attribute data-ph not found")
 	}
-	switch tType {
+	switch kind {
 	case "sMainMeanings":
 		return "main", nil
 	case "sCmpdForms":
@@ -67,16 +142,83 @@ func selectTranslationType(s *goquery.Selection, t *translation) (string, error)
 	}
 }
 
-func parseWRDTable(root *goquery.Selection, t translation) {
-	var interp []interpretation
+// Parses even/odd group rows from table
+func parseTRs(s *goquery.Selection, interp *interpretation) error {
+	DefToSelection := s.RemoveFiltered("span.dsense")
+	rootTR := s.Find("tr[id]")
+	FrWdSelection := rootTR.Find("td.FrWd")
+	ToWdSelection := rootTR.Find("td.ToWd")
+	DefFrSelection := rootTR.Find("td:not([class])")
+	FrExSelection := s.Find("td.FrEx")
+	ToExSelection := s.Find("td.ToEx")
+	FrPosEM := FrWdSelection.RemoveFiltered("em")
+	ToPosEM := ToWdSelection.RemoveFiltered("em")
+	FrLang := FrPosEM.AttrOr("data-lang", "fr")
+	ToLang := ToPosEM.AttrOr("data-lang", "en")
 
-	//loop through siblings and construct interpretations
+	fmt.Printf("to: %v\nfrom: %v\n", DefFrSelection.Text(), DefToSelection.Text())
 
-	return &interp
+	re := regexp.MustCompile(`\(([^)]*)\)`)
+
+	fromMatches := re.FindAllStringSubmatch(DefFrSelection.Text(), -1)
+	//toMatches := re.FindAllStringSubmatch(DefToSelection.Text(), -1)
+
+	var definitions []string
+	for _, match := range fromMatches {
+		if len(match) > 1 {
+			definitions = append(definitions, match[1])
+		}
+	}
+
+	if len(definitions) < 1 {
+		return errors.New("no definitions found")
+	}
+
+	if FrWdSelection.Length() > 1 {
+		return errors.New("more than one tr.FrWd found")
+	}
+	{
+		// add word to "from" slice
+		for _, phrase := range strings.Split(FrWdSelection.Text(), ",") {
+			interp.from = append(interp.from, &word{
+				name:       phrase,
+				language:   FrLang,
+				pos:        FrPosEM.Text(),
+				definition: definitions[0],
+			})
+		}
+		//add words to "to" slice
+		ToWdSelection.Each(func(i int, s *goquery.Selection) {
+			for _, phrase := range strings.Split(s.Text(), ",") {
+				interp.to = append(interp.to, &word{
+					name:       phrase,
+					language:   ToLang,
+					pos:        ToPosEM.Text(),
+					definition: definitions[0],
+				})
+			}
+		})
+		//add "from" expressions to examples
+		FrExSelection.Each(func(i int, s *goquery.Selection) {
+			interp.examples = append(interp.examples, &example{
+				language: FrLang,
+				phrase:   s.Text(),
+			})
+		})
+		//add "to" expressions to examples
+		ToExSelection.Each(func(i int, s *goquery.Selection) {
+			interp.examples = append(interp.examples, &example{
+				language: ToLang,
+				phrase:   s.Text(),
+			})
+		})
+
+	}
+	return nil
 }
 
 // id=articleWRD
-func getWRTranslation(word string) translation {
+func getWRFTranslationHTTP(word string) *translation {
 	user_agent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://www.wordreference.com/fren/%s", word), nil)
 	if err != nil {
@@ -98,8 +240,40 @@ func getWRTranslation(word string) translation {
 		log.Fatal(err)
 	}
 
-	return parseRoot(doc.Find("table.WRD"))
+	return parseTables(doc.Find("table.WRD"))
+}
+
+func getWRTRanslationFile(fileName string) {
+	// create from a file
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// use the goquery document...
+	parseTables(doc.Find("table.WRD"))
 }
 func main() {
-	getWRTranslation("coucou")
+
+	getWRTRanslationFile("cuire.html")
+	//translation := getWRFTranslationHTTP("coucou")
+	/*
+		for _, interp := range translation.interpretations {
+			fmt.Printf("%s translations", interp.kind)
+			fmt.Printf("--------------------------------------------")
+			for _, from := range interp.from {
+				fmt.Printf("%s (%s): %s", from.name, from.pos, from.definition)
+			}
+			for _, to := range interp.to {
+				fmt.Printf("%s (%s): %s", to.name, to.pos, to.definition)
+			}
+			for _, example := range interp.examples {
+				fmt.Printf("%s", example)
+			}
+		}
+	*/
 }
